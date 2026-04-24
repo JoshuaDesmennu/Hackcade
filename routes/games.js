c1741c538f8b7828630a81b11e6305f33fa6525e
Oluwatayo <jaymanify1@gmail.com>
2026-04-24 02:27:35 +0000

    Added basic network capability for tictactoe, is a bit buggy though

--- routes/games.js
+++ routes/games.js
@@ -2,11 +2,15 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/tictactoe", (req, res) => {
-    res.render("tictactoe");
+    res.render("tictactoe", {
+        title: "TicTacToe",
+    });
 })
 
 router.get("/checkers", (req, res) => {
-    res.render("checkers");
+    res.render("checkers", {
+        title: "Checkers - Draughts - Damme"
+    });
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,170 @@
 const express = require("express");
+const http = require("http");
+const websocket = require("ws");
 const app = express();
+
+const server = http.createServer(app);
+const wss = new websocket.Server({ server });
+
+let games = {
+    checkers: {
+        name: "Checkers",
+        maxPlayersPerRoom: 2,
+        totalRooms: 0,
+    },
+    tictactoe: {
+        name: "TicTacToe",
+        maxPlayersPerRoom: 2,
+        totalRooms: 0,
+    },
+};
+
+let rooms = {};
+/**
+ * The structure for each member is
+ * Room code: Room code
+ * number of players
+ * game name
+ */
+
+wss.on("connection", (socket) => {
+    console.log("Just got a new connection");
+    socket.on("message", (data) => {
+        const jsonData = JSON.parse(data);
+        if (jsonData["event"] === "roomconnect") {
+            // deny connection if already connected
+            if (socket.isConnected === true) {
+                socket.send(
+                    JSON.stringify({
+                        event: "roomnotconnected",
+                        reason: "already connected",
+                    }),
+                );
+            }
+            // deny access if room alr exists with a different game
+            if (rooms[jsonData["roomCode"]] != null) {
+                if (
+                    rooms[jsonData["roomCode"]].gameName != null &&
+                    rooms[jsonData["roomCode"]].gameName !==
+                        jsonData["gameName"]
+                ) {
+                    socket.send(
+                        JSON.stringify({
+                            event: "roomnotconnected",
+                            reason: "room occupied",
+                        }),
+                    );
+                    return;
+                }
+
+                // deny access if room is full
+                if (
+                    rooms[jsonData["roomCode"]].numberOfPlayers >=
+                    games[jsonData["gameName"]].maxPlayersPerRoom
+                ) {
+                    socket.send(
+                        JSON.stringify({
+                            event: "roomnotconneted",
+                            reason: "room full",
+                        }),
+                    );
+                    return;
+                }
+            }
+
+            console.log(jsonData);
+            // update or create room with new info
+            rooms[jsonData["roomCode"]] = {
+                ...rooms[jsonData["roomCode"]],
+                roomCode: jsonData["roomCode"],
+                numberOfPlayers:
+                    rooms[jsonData["roomCode"]] == null
+                        ? 1
+                        : rooms[jsonData["roomCode"]].numberOfPlayers + 1,
+                gameName: jsonData["gameName"],
+            };
+
+            if (rooms[jsonData["roomCode"]].numberOfPlayers == 1) {
+                rooms[jsonData["roomCode"]].gameState =
+                    jsonData["initialState"];
+            }
+
+            socket.roomCode = jsonData["roomCode"];
+            socket.isConnected = true;
+
+            if (rooms[jsonData["roomCode"]].sockets == null) {
+                rooms[jsonData["roomCode"]].sockets = [socket];
+            } else {
+                rooms[jsonData["roomCode"]].sockets.push(socket);
+            }
+            console.log(rooms);
+
+            socket.send(
+                JSON.stringify({
+                    event: "roomconnected",
+                    roomCode: jsonData["roomCode"],
+                    numberOfPlayers:
+                        rooms[jsonData["roomCode"]].numberOfPlayers,
+                    gameState: rooms[jsonData["roomCode"]].gameState,
+                }),
+            );
+        } else if (jsonData["event"] == "broadcastroom") {
+            if (rooms[jsonData["roomCode"]].sockets != null) {
+                rooms[jsonData["roomCode"]].sockets
+                    .filter((s) => s != socket)
+                    .forEach((s) => {
+                        console.log("Sent");
+                        s.send(JSON.stringify(jsonData["body"]));
+                    });
+            }
+        } else if (jsonData["event"] == "updatestate") {
+            if (
+                jsonData["gameState"] != null &&
+                jsonData["gameState"].board != null &&
+                jsonData["gameState"].currentPlayer != null
+            ) {
+                rooms[jsonData["roomCode"]].gameState = jsonData["gameState"];
+            }
+        }
+    });
+
+    socket.on("close", (code) => {
+        console.log(`Disconnected with code ${code}`);
+        if (socket.isConnected === true) {
+            rooms[socket.roomCode].sockets = rooms[
+                socket.roomCode
+            ].sockets.filter((s) => s != socket);
+            if (--rooms[socket.roomCode].numberOfPlayers === 0) {
+                rooms[socket.roomCode] = null;
+            }
+            socket.isConnected = false;
+            socket.roomCode = null;
+        }
+    });
+});
 
 app.use(express.static("static"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("home");
-}); 
+    res.render("home", { title: "Home" });
+});
+
+app.get("/numberinroom/:roomCode", (req, res) => {
+    if (rooms[req.params.roomCode] == null) {
+        res.json({
+            event: "numberinroomfailed",
+            reason: "room not existing",
+        });
+    } else {
+        res.json({
+            event: "givenumberinroom",
+            number: rooms[req.params.roomCode].numberOfPlayers,
+        });
+    }
+});
 
 const gamesRouter = require("./routes/games");
 app.use("/games", gamesRouter);
 
-app.listen(3000);
+server.listen(3000, "localhost");
